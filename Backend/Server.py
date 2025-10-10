f2e3c91e7c8e89cd21b9ba2c7eadd9afaf67a716
programmaraditya123 <officialadi4255@gmail.com>
2025-10-10 18:10:45 +0530

    add server.py for mcp server

---
+++ Backend/Server.py
@@ -0,0 +1,11 @@
+from fastmcp import FastMCP
+from main import app
+
+mcp = FastMCP.from_fastapi(
+    app=app,
+    name="All rounder mcp server",
+    exclude=["/chat","/download","/upload","/ping"]
+)
+
+if __name__=="__main__":
+    mcp.run(transport="http",host="0.0.0.0",port=8000)
